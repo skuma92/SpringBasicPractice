87ac2e896698a65be785a56965c62add15da124d
skuma92 <87821778+skuma92@users.noreply.github.com>
2022-03-18 00:23:48 +0530

    SpringServiceTest created

---
+++ src/main/java/com/shivam/service/SpringServiceTest.java
@@ -0,0 +1,17 @@
+package com.shivam.service;
+
+public class SpringServiceTest {
+
+    public SpringServiceTest(){
+        System.out.println("in constructor");
+    }
+
+    public void method1(){
+        System.out.println("in method 1");
+    }
+
+    public void method2(){
+        System.out.println("in method 2");
+    }
+
+}
