083516a2853e2595f4e9a498a2aac8d46d93185a
skuma92 <87821778+skuma92@users.noreply.github.com>
2022-03-18 00:24:31 +0530

    SprintServiceTestNew created

---
+++ src/main/java/com/shivam/service/SprintServiceTestNew.java
@@ -0,0 +1,11 @@
+package com.shivam.service;
+
+public class SprintServiceTestNew extends SpringServiceTest{
+
+    public SprintServiceTestNew(){
+        System.out.println("in constructo of SprintServiceTestNew");
+    }
+    public void methodNew1(){
+        System.out.println("in methodNew1");
+    }
+}
